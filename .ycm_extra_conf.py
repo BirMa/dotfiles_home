85036c790575b32a226add9e5b688e936276c9c9
Birnthaler Martin <martin_os@birnthaler.de>
2015-07-24 13:37:35 +0000

    Refactor clang flags to reflect my usecase.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -24,30 +24,25 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
+# General flags:
 '-Wall',
 '-Wextra',
 '-Werror',
 '-fexceptions',
-'-DNDEBUG',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
-]
 
+# Defines:
+#'-DNDEBUG',
+
+# Language standard:
+'-std=c++11',
+#'-libstd=???',
+'-xc++',
+
+# Includes:
+'-isystem/usr/include/',
+
+#'-I./',
+]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
